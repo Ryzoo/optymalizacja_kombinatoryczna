93f0258c06f1f35bb8b1a89d83345d6a11c33f43
Grzegorz Kastelik <grzegorz.kastelik@euvic.pl>
2020-10-25 11:13:58 +0100

    improve

--- Z1/src/Algorithm.js
+++ Z1/src/Algorithm.js
@@ -2,6 +2,7 @@ class Algorithm{
     constructor(){
         this.combinationBase = []
         this.toCheckCombinationBase = []
+        this.best = {}
     }
 
     async prepareData(verticesWithEdges){
@@ -12,31 +13,53 @@ class Algorithm{
     }
 
     async addNextToCombination(solution, combination){
-        if(!combination.length) return solution
+        if(!combination.length) return [solution]
 
-        const nextElement = combination[0]
-        const nextElementConnectedVerticles = this.verticesWithEdges[nextElement].right
-        const nextSolution = [...solution, nextElement]
-        const nextCombination = combination
-            .filter((x) => !nextElementConnectedVerticles.includes(x) && x !== nextElement)
-            .filter((y) => nextSolution.every((z) => !this.verticesWithEdges[y].right.includes(z)))
-    
-        return await this.addNextToCombination(nextSolution, nextCombination)
+        let results = []
+        let beforeFirstFromCombination = null
+        let beforeLastFromSolution = null
+
+        await Promise.all(combination.map(async (c) => {
+            const nextSolution = [...solution, c]
+            const nextCombination = combination
+                .filter((y) => y > c 
+                    && !this.verticesWithEdges[y].right.some((z) => nextSolution.includes(z)) 
+                    && nextSolution.every((z) => !this.verticesWithEdges[z].right.includes(y)) 
+                    && y !== c)
+
+            const reversed = [...nextCombination].reverse()
+            const testedCombination = reversed
+                .filter((y) => !nextCombination.length || !this.verticesWithEdges[y].left.some((z) => z === nextCombination[0]))
+                .reverse()
+
+            const firstFromCombination = testedCombination.length ? testedCombination[0] : null
+            const lastFromSolution = nextSolution.length ? nextSolution[nextSolution.length-1] : null
+
+            if(beforeFirstFromCombination && beforeLastFromSolution && firstFromCombination === beforeLastFromSolution && lastFromSolution === beforeFirstFromCombination){
+                return;
+            }
+
+            beforeFirstFromCombination = firstFromCombination
+            beforeLastFromSolution = lastFromSolution
+            
+            let requirements = []
+            nextSolution.forEach((x) => requirements = [...requirements, ...this.verticesWithEdges[x].right])
+            const requirementsChecker = [...nextSolution, ...testedCombination]
+            const test2Passed = requirements
+                .every((v) => this.verticesWithEdges[v].right.some((v) => requirementsChecker.includes(v)))
+
+            if(test2Passed){
+                const result = await this.addNextToCombination(nextSolution, nextCombination)
+                results = [...results, ...result]
+            } else results = [...results, nextSolution]
+          }));
+
+        return results
     }
 
     async prepareSolutions(){
-        let listOfSolutions = []
-    
-        await Promise.all(this.toCheckCombinationBase.map(async (x) => {
-            const nextElementConnectedVerticles = this.verticesWithEdges[x].right
-            const baseSoulution = [...this.combinationBase, x]
-            const newCombination = this.toCheckCombinationBase
-                .filter((y) => !nextElementConnectedVerticles.includes(y) && x !== y)
-                .filter((y) => !baseSoulution.some((z) => this.verticesWithEdges[y].right.includes(z)))
-            const result = await this.addNextToCombination(baseSoulution, newCombination)
-            listOfSolutions.push(result)
-          }));
-    
+        let listOfSolutions = [...await this.addNextToCombination(this.combinationBase, this.toCheckCombinationBase)]
+
         this.solutions = listOfSolutions
             .map((x) => [...new Set(x)])
             .filter((x) => this.checkIsAnyConnected(x))
@@ -45,6 +68,7 @@ class Algorithm{
     
     checkIsAnyConnected(result){
         const notInResult = this.toCheckCombinationBase.filter((x) => !result.includes(x))
+        // const real = [5, 11, 16, 25, 34, 44, 46, 54, 56, 94, 102, 109, 127, 134, 135, 143, 153]
         return notInResult.every((nr) => this.verticesWithEdges[nr].right.some((v) => result.includes(v)))
     }
 
